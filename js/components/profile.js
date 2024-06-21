d1bdfa8d9a0d13930b86aa94def3e8fc46fabd5f
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-21 04:19:49 -0500

    feat: :sparkles: Crear función asíncrona llamada perfil en archivo profile.js
    Se ha creado una nueva función asíncrona llamada perfil en la ruta js/components en el archivo profile.js.

---
+++ js/components/profile.js
@@ -0,0 +1,14 @@
+export const profile = async (objData) => {
+    return objData.map((person) => {
+        return `
+        <div class="person__profile">
+            <img src="${person.avatar}" alt="img">
+            <div class="profile__info">
+                <h2>${person.fullName}</h2>
+                <p>${person.description}</p>
+            </div>
+        </div>
+        <hr>
+        `;
+    }).join('');
+};
