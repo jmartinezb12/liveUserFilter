ab5dd3f1727cdce667256731e0c2abccc8c23983
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-21 03:46:59 -0500

    feat: :sparkles: Implementar método GET para obtener todas las personas
    Se ha implementado el método GET en el archivo app.js en la ruta js/modules/ que permite obtener todas las personas mediante la función getAllPeople y el endpoint https://6674179975872d0e0a950e53.mockapi.io/user.

---
+++ js/modules/app.js
@@ -0,0 +1,9 @@
+export const getAllPeople = async () => {
+
+    const url = ("https://6674179975872d0e0a950e53.mockapi.io/user");
+    const custom = { method: "GET" };
+
+    let res = await fetch(url, custom);
+    let data = await res.json();
+    return data;
+}
