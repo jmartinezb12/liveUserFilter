a87e72f87be44cab06bb4937e6443e6a8da979b3
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-21 04:40:46 -0500

    feat: :sparkles: Crear script de búsqueda en archivo main.js
    Se ha creado un script en el archivo main.js que permite la búsqueda de las personas en la ruta js.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 import { getAllPeople } from "./modules/app.js";
-import { profiles } from "./components/profile.js";
+import { profile } from "./components/profile.js";
 
 const search = document.querySelector("#input_search");
 const main = document.querySelector(".main");
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const people = await getAllPeople();
+
+    main.innerHTML = await profile(people);// Cargar personas por defecto
+
+    search.addEventListener("input", async (event) => {
+        const searchTerm = event.target.value.toLowerCase().trim(); // Obtener el valor del input y convertirlo a minusculas y sin espacios
+
+        const filteredProfiles = people.filter((person) => {
+            return person.fullName.toLowerCase().includes(searchTerm); //Comparar si el nombre de la persona incluye el término de búsqueda
+        });
+
+        if (filteredProfiles.length === 0) { // Si no se encuentran personas
+            main.innerHTML = /*html*/ `<h2>No results found</h2>
+                <style>.main{display: flex; justify-content: center; align-items: center; margin-top: 50px; color: rgb(13, 160, 106)}</style>`;
+        } else{
+            main.innerHTML = await profile(filteredProfiles);
+        }
+    });
+})
