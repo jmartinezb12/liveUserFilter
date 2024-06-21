3935c67ca5c01de09ea5b796486f82ca74caf2cb
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-21 04:01:50 -0500

    feat: :sparkles: Implementar variables en archivo main.js
    Se han implementado las variables en el archivo main.js para todo el script.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { getAllPeople } from "./modules/app.js";
+import { profiles } from "./components/profile.js";
+
+const search = document.querySelector("#input_search");
+const main = document.querySelector(".main");
